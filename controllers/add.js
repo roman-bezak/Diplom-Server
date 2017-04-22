7431082f3cfa728ef7d7267c50a807413e9584a9
Roman Bezak <r.bezak77@gmail.com>
2017-04-23 01:23:10 +0300

    go to sleep

--- controllers/add.js
+++ controllers/add.js
@@ -1,8 +1,20 @@
 var express = require('express')
   , router = express.Router()
+  , d = require('../db')
 
 router.get('/', function(req, res) {
   res.render('add/add')
+})
+
+router.post("/", function (req, res) {
+    console.log(req.body)
+    function Tree(name) {
+      this.name = name;
+    }
+
+    var theTree = new Tree("Красное дерево");
+    d.get().collection('new').insert(theTree);
+    res.redirect('/')
 })
 
 module.exports = router
