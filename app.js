ca352dd3cabea1ceaa0d4c7293ee4c81b24d67d3
Roman Bezak <r.bezak77@gmail.com>
2017-04-22 23:46:39 +0300

    add bootstrap lib

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// var express = require('express')
-//   , app = express()
-//
-// var db = require('./db')
-//
-// app.engine('jade', require('jade').__express)
-// app.set('view engine', 'jade')
-//
-// // app.use('/comments', require('./controllers/comments'))
-// // app.use('/users', require('./controllers/users'))
-//
-// // Connect to Mongo on start
-// db.connect('mongodb://localhost:27017/diplom-server-db', function(err) {
-//   if (err) {
-//     console.log('Unable to connect to Mongo.')
-//     process.exit(1)
-//   }
-// })
-//
-//
-// app.listen(3000, function() {
-//   console.log('Listening on port 3000...')
-// })
-
-
 var express = require('express')
   , app = express()
   , db = require('./db')
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,14 @@
 var express = require('express')
   , router = express.Router()
 
-router.use('/add', require('./addproject'))
+router.use('/add', require('./add'))
 
 router.get('/', function(req, res) {
   res.render('index')
 })
 
 router.get('*', function(req, res) {
-  res.status(404);
-  res.render('404')
+    res.status(404).render('404');
 })
 
 
